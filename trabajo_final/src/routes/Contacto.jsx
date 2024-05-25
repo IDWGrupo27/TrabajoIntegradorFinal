a86dc5441467ac715d2b4094a278372da01f5836
martinCdia <forissimartin@gmail.com>
2024-05-25 10:37:36 -0300

    Modificacion formulario contacto

--- trabajo_final/src/routes/Contacto.jsx
+++ trabajo_final/src/routes/Contacto.jsx
@@ -1,57 +1,84 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "../css/Contacto.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShareFromSquare } from "@fortawesome/free-solid-svg-icons";
-import { MensajeEnviado } from "./MensajeEnviado";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 
 export const Contacto = () => {
 
-    
+    const [mensaje, setMensaje] = useState({
+        nombre:"",
+        apellido:"",
+        email:"",
+    });
 
+    const navigate = useNavigate();
+
+    const toNavigate = () => {
+        navigate(
+            "/mensaje-enviado",
+            {state:{
+                nombre:mensaje.nombre, 
+                apellido:mensaje.apellido, 
+                email:mensaje.email}
+            }
+        );
+    };
+    
     return (
         <main>
             <section className="contacto">
-                <form action="submit">
+
+                <form action="submit" onSubmit={toNavigate}>
+
                     <h1>FORMULARIO DE CONTACTO</h1>
+
                     <div className="input-group">
-                        <label for="name" className="required">
+                        <label htmlFor="name" className="required">
                             Nombre:
                         </label>
                         <input
+                            id="name"
                             className="inpt"
                             type="text"
                             name="name"
                             placeholder="Nombre..."
+                            onChange={(e) => setMensaje({ ...mensaje, nombre: e.target.value})}
                             required
                         />
 
-                        <label for="last_name" className="required">
+                        <label htmlFor="last_name" className="required">
                             Apellido:
                         </label>
                         <input
+                            id="last_name"
                             className="inpt"
                             type="text"
                             name="last_name"
                             placeholder="Apellido..."
+                            onChange={(e) => setMensaje({ ...mensaje, apellido: e.target.value})}
                             required
                         />
 
-                        <label for="email" className="required">
+                        <label htmlFor="email" className="required">
                             Email:
                         </label>
                         <input
+                            id="email"
                             className="inpt"
                             type="email"
                             name="email"
                             placeholder="Email..."
+                            onChange={(e) => setMensaje({ ...mensaje, email: e.target.value})}
                             required
                         />
 
-                        <label for="message" className="required">
+                        <label htmlFor="message" className="required">
                             Mensaje:
                         </label>
                         <textarea
+                            id="message"
                             cols="30"
                             rows="5"
                             name="message"
@@ -59,10 +86,10 @@ export const Contacto = () => {
                         ></textarea>
                     </div>
 
-                    <Link className="btn" to={"/mensaje-enviado"}>
-                        <FontAwesomeIcon icon={faShareFromSquare}/>&nbsp; Enviar
-                    </Link>
-
+                    <button className="btn"><FontAwesomeIcon icon={faShareFromSquare}/>
+                        &nbsp; Enviar
+                    </button>
+                
                 </form>
                 
             </section>
--- trabajo_final/src/routes/MensajeEnviado.jsx
+++ trabajo_final/src/routes/MensajeEnviado.jsx
@@ -3,20 +3,23 @@ import { Link } from "react-router-dom";
 import "../css/Contacto.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { useLocation } from "react-router-dom";
 
 export const MensajeEnviado = () => {
+
+    const mensaje = useLocation();
+    const {nombre, apellido, email} = mensaje.state;
+
     return (
         <main>
-            <section class="contacto">
-                <div class="send">
-                    <h1 class="mensaje">
-                        <p>Hemos recibido su mensaje.</p>
-
-                        <p>Nos contactaremos a la brevedad!</p>
-
-                        <p>Gracias!!</p>
+            <section className="contacto">
+                <div className="send">
+                    <h1 className="mensaje">
+                        <p>Hola <u>{nombre} {apellido}</u>. Hemos recibido su mensaje.</p>
+                        <p>En breve responderemos sus inquietudes a su direccion de email: <u>{email}</u></p>
+                        <p>Gracias por contactarnos!!</p>
                     </h1>
-                    <Link class="btn" to={"/"}>
+                    <Link className="btn" to={"/"}>
                        <FontAwesomeIcon icon={faHome}/>
                     </Link>
                 </div>
