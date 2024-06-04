5b19022011d94febfb03fc5937f8b5f5409ae2ce
Lautaro Lombardo <lautilomba517@gmail.com>
2024-06-04 11:41:37 -0300

    mergeadas ramas y modifcacion en el header

--- trabajo_final/src/components/Header.jsx
+++ trabajo_final/src/components/Header.jsx
@@ -1,36 +1,54 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 import Logo from "../img/logo.png";
 import "../css/Header.css";
 
 export const Header = () => {
+    const pantallaChica = useMediaQuery({ query: "(max-width: 1000px)" });
+
     return (
         <header>
             <div>
                 <Link to={"/"}>
                     <div className="logo">
                         <img src={Logo} alt="IDW alojamiento" height="40" />
-                        <h3>IDW alojamiento</h3>
+                        <span>IDW alojamiento</span>
                     </div>
                 </Link>
             </div>
 
-            <div className="header-menu">
-                <Link className="menu-item" to={"/contacto"}>
-                    <span>Contacto</span>
-                </Link>
-                <Link className="menu-item" to={"/informacion-institucional"}>
-                    <span>Información</span>
-                </Link>
-                <Link className="menu-item" to={"/administracion"}>
-                    <span>Administración</span>
-                </Link>
-            </div>
+            {pantallaChica ? (
+                <button>
+                    <FontAwesomeIcon size="3x" icon={faBars} />
+                </button>
+            ) : (
+                <div className="header-menu">
+                    <Link className="menu-item" to={"/contacto"}>
+                        <span>Contacto</span>
+                    </Link>
+                    <Link
+                        className="menu-item"
+                        to={"/informacion-institucional"}
+                    >
+                        <span>Información</span>
+                    </Link>
+                    <Link className="menu-item" to={"/administracion"}>
+                        <span>Administración</span>
+                    </Link>
+                </div>
+            )}
 
-            <div className="phone-number">
-                <span>Para venta telefónica</span>
-                <span className="number">0800-999-1234</span>
-            </div>
+            {pantallaChica ? null : (
+                <div className="phone-number">
+                    <span>Para venta telefónica</span>
+                    <span className="number">0800-999-1234</span>
+                </div>
+            )}
         </header>
     );
 };
